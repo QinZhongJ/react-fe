f7b7f1007d9383d7ed200a993ef4263a56f453d6
秦中杰 <qinzj01@mingyuanyun.com>
2023-07-11 18:19:44 +0800

    feat: build

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,10 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   server: {
+    // 支持IP访问
+    host: true,
     // 指定dev sever的端口号
     port: 3030,
     // 自动打开浏览器运行以下页面
@@ -21,4 +24,10 @@ export default defineConfig({
     },
   },
   plugins: [react()],
+  build: {
+    // build目录名称，默认为"dist"
+    outDir: 'build',
+    // 静态资源存放目录名称，默认为"assets"
+    assetsDir: 'static',
+  },
 });
